57ef4041a19e47c5da3cd08b44322d5c5ed91200
Valencius Apriady <v.apriady@gmail.com>
2024-09-27 21:08:26 +0700

    fix: revert migration types back

--- migrations/20240926100540_create_vendor_table.sql
+++ migrations/20240926100540_create_vendor_table.sql
@@ -2,14 +2,14 @@
 -- +goose StatementBegin
 CREATE TABLE "vendor" (
   "id" int PRIMARY KEY,
-  "name" varchar2(255),
-  "description" varchar2(255),
+  "name" varchar(255),
+  "description" varchar(255),
   "bp_id" int,
-  "bp_name" varchar2(255),
+  "bp_name" varchar(255),
   "rating"  int,
   "area_group_id" int,
-  "area_group_name" varchar2(255),
-  "sap_code" varchar2(255),
+  "area_group_name" varchar(255),
+  "sap_code" varchar(255),
   "modified_date" timestamp,
   "modified_by" int,
   "dt" date
