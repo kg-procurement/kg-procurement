829e60ac2a3eecf6321ae86f794070f339a5490e
edutjie <edutjitrahardja@gmail.com>
2024-10-09 14:10:23 +0700

    [REFACTOR] create account schema and migration

---
+++ migrations/20241009070631_create_account_table.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE account (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(63) UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE account;
+-- +goose StatementEnd
