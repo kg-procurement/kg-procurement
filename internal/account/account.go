3d8a4504128974dcbf456df459b1783984d90eb5
Eduardus Tjitrahardja <69960893+edutjie@users.noreply.github.com>
2024-10-09 14:25:46 +0700

    PBI 128: Create Account Schema and Migration (#24)
    * [REFACTOR] create account schema and migration
    
    * [REFACTOR] Change id type to varchar

---
+++ internal/account/account.go
@@ -0,0 +1,10 @@
+package account
+
+import "time"
+
+type Account struct {
+	ID        int       `json:"id"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+}
