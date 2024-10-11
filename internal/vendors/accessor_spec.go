1e96c342c27373a4460402411ac5c6ca36bfe8e1
valeelim <valeriansalim@gmail.com>
2024-10-11 22:40:47 +0700

    [REFACTOR]: standardize response output

--- internal/vendors/accessor_spec.go
+++ internal/vendors/accessor_spec.go
@@ -3,6 +3,6 @@ package vendors
 import "kg/procurement/internal/common/database"
 
 type AccessorGetAllPaginationData struct {
-	Vendors  []Vendor
-	Metadata database.PaginationMetadata
+	Vendors  []Vendor                    `json:"vendors"`
+	Metadata database.PaginationMetadata `json:"metadata"`
 }
