3ae222ae8bd02c29bbac331bf2965fa66ca0c01e
ferryhieronimus <ferryhieronimus@gmail.com>
2024-10-18 20:57:19 +0700

    [RED] add test when request does not contains header

--- internal/common/middleware/auth_test.go
+++ internal/common/middleware/auth_test.go
@@ -1,9 +1,55 @@
 package middleware
 
 import (
+	"github.com/gin-gonic/gin"
+	"github.com/onsi/gomega"
+	"go.uber.org/mock/gomock"
+	"kg/procurement/internal/token"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func Test_newAuthMiddleware(t *testing.T) {
 	_ = NewAuthMiddleware(nil)
+}
+
+func TestAuthMiddleware_MustAuthenticated(t *testing.T) {
+	var (
+		g              *gomega.WithT
+		mockCtrl       *gomock.Controller
+		mockTokenMgr   *token.MocktokenManager
+		tokenSvc       *token.TokenService
+		authMiddleware *AuthMiddleware
+	)
+
+	setup := func(t *testing.T) {
+		g = gomega.NewWithT(t)
+		mockCtrl = gomock.NewController(t)
+		mockTokenMgr = token.NewMocktokenManager(mockCtrl)
+		tokenSvc = &token.TokenService{
+			TokenManager: mockTokenMgr,
+		}
+		authMiddleware = NewAuthMiddleware(tokenSvc)
+	}
+
+	teardown := func() {
+		mockCtrl.Finish()
+	}
+
+	t.Run("NoAuthorizationHeaderReturnsError", func(t *testing.T) {
+		setup(t)
+		defer teardown()
+
+		req, _ := http.NewRequest("GET", "/", nil)
+		w := httptest.NewRecorder()
+		c, _ := gin.CreateTestContext(w)
+		c.Request = req
+
+		handler := authMiddleware.MustAuthenticated()
+		handler(c)
+
+		g.Expect(w.Code).To(gomega.Equal(http.StatusBadRequest))
+		g.Expect(w.Body.String()).To(gomega.ContainSubstring(ErrorAuthHeader))
+	})
 }
