48cbed27356242fd3f620384599f651bb5f42f44
ferryhieronimus <ferryhieronimus@gmail.com>
2024-10-18 21:03:40 +0700

    [RED] add test when request contains valid token

--- internal/common/middleware/auth_test.go
+++ internal/common/middleware/auth_test.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"errors"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/onsi/gomega"
 	"go.uber.org/mock/gomock"
 	"kg/procurement/internal/token"
@@ -107,5 +108,39 @@ func TestAuthMiddleware_MustAuthenticated(t *testing.T) {
 
 		g.Expect(w.Code).To(gomega.Equal(http.StatusInternalServerError))
 		g.Expect(w.Body.String()).To(gomega.ContainSubstring("token validation error"))
+	})
+
+	t.Run("ValidTokenReturnsStatusOKAndSetsAuthPayload", func(t *testing.T) {
+		setup(t)
+		defer teardown()
+
+		expectedClaims := &token.Claims{
+			RegisteredClaims: jwt.RegisteredClaims{
+				Subject: "user123",
+			},
+		}
+
+		// mock
+		mockTokenMgr.EXPECT().ValidateToken("validtoken").Return(expectedClaims, nil)
+
+		// create request with valid Bearer token
+		req, _ := http.NewRequest("GET", "/", nil)
+		req.Header.Set(AuthorizationHeader, "Bearer validtoken")
+		w := httptest.NewRecorder()
+		c, _ := gin.CreateTestContext(w)
+		c.Request = req
+
+		// call the middleware
+		handler := authMiddleware.MustAuthenticated()
+		handler(c)
+
+		g.Expect(w.Code).To(gomega.Equal(http.StatusOK))
+
+		// Check if "auth_payload" is set in context
+		value, exists := c.Get("auth_payload")
+		g.Expect(exists).To(gomega.BeTrue())
+		g.Expect(value).To(gomega.Equal(token.ClaimSpec{
+			UserID: "user123",
+		}))
 	})
 }
