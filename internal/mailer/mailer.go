8ac042719edd8fdd91e1eea0a5795964258be2c7
ferryhieronimus <ferryhieronimus@gmail.com>
2024-11-01 10:01:05 +0700

    [GREEN] add implementation for service

--- internal/mailer/mailer.go
+++ internal/mailer/mailer.go
@@ -1,12 +1,21 @@
 //go:generate mockgen -typed -source=mailer.go -destination=mailer_mock.go -package=mailer
 package mailer
 
+import "time"
+
 type Email struct {
 	From    string
 	To      []string
 	CC      []string
 	Subject string
 	Body    string
+}
+
+type EmailStatus struct {
+	ID           string    `db:"id" json:"id"`
+	EmailTo      string    `db:"email_to" json:"email_to"`
+	Status       string    `db:"status" json:"status"`
+	ModifiedDate time.Time `db:"modified_date" json:"modified_date"`
 }
 
 type EmailProvider interface {
