5328b55843e490703214c6130625dc685dec7677
Hieronimus Ferry <101467366+ferryhieronimus@users.noreply.github.com>
2024-11-08 20:53:01 +0700

    PBI-150-151: Email Status (#55)
    * [RED] add migration
    
    * [RED] add test for accessor
    
    * [RED] add test for service
    
    * [GREEN] add implementation for accessor
    
    * [GREEN] add implementation for service
    
    * [GREEN] add more test case and increase coverage
    
    * [REFACTOR] change variable name to be more concise

---
+++ internal/mailer/accessor_test.go
@@ -0,0 +1,131 @@
+package mailer
+
+import (
+	"context"
+	"database/sql"
+	"database/sql/driver"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/benbjohnson/clock"
+	"github.com/jmoiron/sqlx"
+	"github.com/onsi/gomega"
+	"regexp"
+	"testing"
+)
+
+func Test_newPostgresEmailStatusAccessor(t *testing.T) {
+	_ = newPostgresEmailStatusAccessor(nil, nil)
+}
+
+func Test_WriteEmailStatus(t *testing.T) {
+	t.Parallel()
+
+	t.Run("success", func(t *testing.T) {
+		var (
+			ctx     = context.Background()
+			c       = setupEmailStatusAccessorTestComponent(t, WithQueryMatcher(sqlmock.QueryMatcherRegexp))
+			now     = c.cmock.Now()
+			emailStatus = EmailStatus{
+				ID:           "123",
+				EmailTo:      "email@email.com",
+				Status:       "sent",
+				ModifiedDate: now,
+			}
+		)
+
+		transformedQuery, args, _ := sqlx.Named(insertEmailStatus, emailStatus)
+		driverArgs := make([]driver.Value, len(args))
+		for i, arg := range args {
+			driverArgs[i] = arg
+		}
+
+		c.mock.ExpectExec(regexp.QuoteMeta(transformedQuery)).WithArgs(
+			driverArgs...,
+		).WillReturnResult(sqlmock.NewResult(1, 1))
+
+		err := c.accessor.WriteEmailStatus(ctx, emailStatus)
+		c.g.Expect(err).Should(gomega.BeNil())
+	})
+
+	t.Run("returns error on db failure", func(t *testing.T) {
+		var (
+			ctx     = context.Background()
+			c       = setupEmailStatusAccessorTestComponent(t)
+			now     = c.cmock.Now()
+			emailStatus = EmailStatus{
+				ID:           "123",
+				EmailTo:      "email@email.com",
+				Status:       "sent",
+				ModifiedDate: now,
+			}
+		)
+
+		transformedQuery, args, _ := sqlx.Named(insertEmailStatus, emailStatus)
+		driverArgs := make([]driver.Value, len(args))
+		for i, arg := range args {
+			driverArgs[i] = arg
+		}
+
+		c.mock.ExpectExec(regexp.QuoteMeta(transformedQuery)).WithArgs(
+			driverArgs...,
+		).WillReturnError(sql.ErrConnDone)
+
+		err := c.accessor.WriteEmailStatus(ctx, emailStatus)
+		c.g.Expect(err).Should(gomega.HaveOccurred())
+	})
+}
+
+func Test_Close(t *testing.T) {
+	t.Parallel()
+
+	t.Run("success", func(t *testing.T) {
+		c := setupEmailStatusAccessorTestComponent(t)
+
+		c.mock.ExpectClose()
+		err := c.accessor.Close()
+		c.g.Expect(err).ShouldNot(gomega.HaveOccurred())
+	})
+}
+
+type emailStatusAccessorTestComponent struct {
+	g        *gomega.WithT
+	mock     sqlmock.Sqlmock
+	db       *sql.DB
+	accessor *postgresEmailStatusAccessor
+	cmock    *clock.Mock
+}
+
+type setupOptions struct {
+	queryMatcher sqlmock.QueryMatcher
+}
+
+func WithQueryMatcher(matcher sqlmock.QueryMatcher) Option {
+	return func(o *setupOptions) {
+		o.queryMatcher = matcher
+	}
+}
+
+type Option func(*setupOptions)
+
+func setupEmailStatusAccessorTestComponent(t *testing.T, opts ...Option) emailStatusAccessorTestComponent {
+	options := setupOptions{
+		queryMatcher: sqlmock.QueryMatcherEqual,
+	}
+
+	for _, opt := range opts {
+		opt(&options)
+	}
+
+	g := gomega.NewWithT(t)
+	db, sqlMock, _ := sqlmock.New(sqlmock.QueryMatcherOption(options.queryMatcher))
+	sqlxDB := sqlx.NewDb(db, "sqlmock")
+
+	clockMock := clock.NewMock()
+
+	return emailStatusAccessorTestComponent{
+		g:        g,
+		mock:     sqlMock,
+		db:       db,
+		accessor: newPostgresEmailStatusAccessor(sqlxDB, clockMock),
+		cmock:    clockMock,
+	}
+}
